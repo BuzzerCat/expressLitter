aea3609114eb1a4a8929ae766a9c13ea2cc7f429
BuzzerCat <buzzerthecat@gmail.com>
2014-09-27 16:19:44 -0500

    App must be defined before it is configured.
    In response to the error message:  cannot read property configure of undefined.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express')
   , passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 
+var app = express();
+
 app.configure(function() {
    app.use(express.session({ secret: 'keyboard cat'}));
 });
-
-var app = express();
 
 app.get('/login', function(req, res){
   res.render('login', { user: req.user, message: req.session.messages });
