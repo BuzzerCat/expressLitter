9ca7b8a17fb2cf3192f225fc6eb66b3362087a83
BuzzerCat <buzzerthecat@gmail.com>
2014-09-28 17:46:00 -0500

    adding an account  route
    The layout page has a link to an account page adding the route to the account page now

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ app.get('/', function(req, res){
   res.render('index', { user: req.user });
 });
 
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
 
 app.get('/login', function(req, res){
   res.render('login', { user: req.user, message: req.session.messages });
