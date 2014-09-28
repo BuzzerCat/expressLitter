ef8497e141f6bcf5d28f2a0612e49235e64a8bb3
BuzzerCat <buzzerthecat@gmail.com>
2014-09-28 11:32:05 -0500

    Created an array as a fake database
    The users array is fake database.

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express')
   , LocalStrategy = require('passport-local').Strategy;
 
 
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+
 function findByUsername(username, fn) {
   for (var i = 0, len = users.length; i < len; i++) {
     var user = users[i];
