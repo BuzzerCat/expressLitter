2d5f604f4b1540fecf5990f131ea1302bb92517f
BuzzerCat <buzzerthecat@gmail.com>
2014-09-28 10:03:19 -0500

    Attempting an alternate route:
    Trying to get rid of this unhelpful error message: 
    
    TypeError: undefined is not a function
       at allFailed (/root/expressLitter/node_modules/passport/lib/middleware/authenticate.js:111:15)
       at attempt (/root/expressLitter/node_modules/passport/lib/middleware/authenticate.js:160:28)
       at Strategy.strategy.fail (/root/expressLitter/node_modules/passport/lib/middleware/authenticate.js:277:9)
       at Strategy.authenticate (/root/expressLitter/node_modules/passport-local/lib/strategy.js:75:17)
       at attempt (/root/expressLitter/node_modules/passport/lib/middleware/authenticate.js:341:16)
       at authenticate (/root/expressLitter/node_modules/passport/lib/middleware/authenticate.js:342:7)
       at callbacks (/root/expressLitter/node_modules/express/lib/router/index.js:164:37)
       at param (/root/expressLitter/node_modules/express/lib/router/index.js:138:11)
       at pass (/root/expressLitter/node_modules/express/lib/router/index.js:145:5)
       at Router._dispatch (/root/expressLitter/node_modules/express/lib/router/index.js:173:5)

--- app.js
+++ app.js
@@ -25,11 +25,20 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user, message: req.session.messages });
 });
 
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-  function(req, res) {
-    res.redirect('/');
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err) }
+    if (!user) {
+      req.session.messages =  [info.message];
+      return res.redirect('/login')
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/');
+    });
+  })(req, res, next);
 });
+
 
 
 app.listen(4000);
