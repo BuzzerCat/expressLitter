189ce0e7f367ebf14cff8d88de900a8c23f0988b
BuzzerCat <buzzerthecat@gmail.com>
2014-09-28 11:16:08 -0500

    Adding to Kat

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ var express = require('express')
 passport.use(new LocalStrategy(
   function(username, password, done) {
    console.log(  "local strategy used.");
+     process.nextTick(function () {
+      
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+
 
 }));
 
