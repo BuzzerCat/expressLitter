2c230dd19ea3a4a8c2147458a35c853c58bd887a
BuzzerCat <buzzerthecat@gmail.com>
2014-09-28 11:55:55 -0500

    Fixing find by ID air
    ReferenceError: findById is not defined
       at /root/expressLitter/app.js:50:3
       at pass (/root/expressLitter/node_modules/passport/lib/authenticator.js:353:9)
       at Authenticator.deserializeUser (/root/expressLitter/node_modules/passport/lib/authenticator.js:358:5)
       at SessionStrategy.authenticate (/root/expressLitter/node_modules/passport/lib/strategies/session.js:49:28)
       at attempt (/root/expressLitter/node_modules/passport/lib/middleware/authenticate.js:341:16)
       at Object.authenticate [as handle] (/root/expressLitter/node_modules/passport/lib/middleware/authenticate.js:342:7)
       at next (/root/expressLitter/node_modules/express/node_modules/connect/lib/proto.js:169:15)
       at Object.initialize [as handle] (/root/expressLitter/node_modules/passport/lib/middleware/initialize.js:62:5)
       at next (/root/expressLitter/node_modules/express/node_modules/connect/lib/proto.js:169:15)
       at multipart (/root/expressLitter/node_modules/express/node_modules/connect/lib/middleware/multipart.js:94:61)

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ var users = [
   , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
 ];
 
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
 
 function findByUsername(username, fn) {
   for (var i = 0, len = users.length; i < len; i++) {
